946992ad3bd560901c0259bb9fe9201cb81edbd4
Sally-SH <gkstmdgml211@kaist.ac.kr>
2023-05-06 18:03:55 +0900

    implement bias only model

---
+++ bias_only.py
@@ -0,0 +1,222 @@
+from unittest.mock import _patch_dict
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dtype = torch.long
+
+from einops import rearrange, repeat, reduce
+from einops.layers.torch import Rearrange
+
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, d_model, nhead, dropout_ratio):
+        super().__init__()
+
+        self.d_model = d_model # d_model
+        self.nhead = nhead # nhead 
+        self.head_dim = d_model // nhead # head_dim 
+
+        self.qLinear = nn.Linear(d_model, d_model) # query lineqr
+        self.kLinear = nn.Linear(d_model, d_model) # key linear
+        self.vLinear = nn.Linear(d_model, d_model) # value linear
+
+        self.oLinear = nn.Linear(d_model, d_model) # output linear
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, query, key, value):
+
+        batch_size = query.shape[0]
+        query_len = query.shape[1]
+        value_len = key_len = key.shape[1]
+
+        # query: [batch_size, query_len, d_model]
+        # key: [batch_size, key_len, d_model]
+        # value: [batch_size, value_len, d_model]
+ 
+        Q = self.qLinear(query)
+        K = self.kLinear(key)
+        V = self.vLinear(value)
+
+        # Q: [batch_size, query_len, d_model]
+        # K: [batch_size, key_len, d_model]
+        # V: [batch_size, value_len, d_model]
+
+        # d_model = nhead * head_dim
+        Q = rearrange(Q, 'b l (h d) -> b h l d', h=self.nhead)
+        K = rearrange(K, 'b l (h d) -> b h l d', h=self.nhead)
+        V = rearrange(V, 'b l (h d) -> b h l d', h=self.nhead)
+
+        # Q: [batch_size, nhead, query_len, head_dim]
+        # K: [batch_size, nhead, key_len, head_dim]
+        # V: [batch_size, nhead, value_len, head_dim]
+
+        #=========Scaled Dot-Product Attention=========
+        weight = torch.matmul(Q, rearrange(K, 'b h l d -> b h d l')) / np.sqrt(self.head_dim)
+
+        # weight: [batch_size, nhead, query_len, key_len]
+        attention = torch.softmax(weight, dim=-1)
+        # attention: [batch_size, nhead, query_len, key_len]
+        c = torch.matmul(self.dropout(attention), V)
+        # c: [batch_size, nhead, query_len, head_dim]
+
+        #=========Scaled Dot-Product Attention=========
+        # reshape & concat
+        c = rearrange(c, 'b h l d -> b l (h d)')
+        # c: [batch_size, query_len, d_model]
+        output = self.oLinear(c)
+      # output: [batch_size, query_len, d_model]
+        return output, attention
+    
+class PositionWiseFeedForwardLayer(nn.Module):
+    def __init__(self, d_model, ff_dim, dropout_ratio):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, ff_dim)
+        self.linear2 = nn.Linear(ff_dim, d_model)
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, x):
+
+        # x: [batch_size, seq_len, d_model]
+        # The MLP contains two layers with a GELU non-linearity
+        x = self.dropout(nn.functional.gelu(self.linear1(x)))
+        # x: [batch_size, seq_len, ff_dim]
+        x = self.linear2(x)
+        # x: [batch_size, seq_len, d_model]
+        return x
+    
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, nhead, ff_dim, dropout_ratio):
+        super().__init__()
+
+        self.layerNorm1 = nn.LayerNorm(d_model)
+        self.layerNorm2 = nn.LayerNorm(d_model)
+        self.multiHeadAttentionLayer = MultiHeadAttentionLayer(d_model, nhead, dropout_ratio)
+        self.positionWiseFeedForward = PositionWiseFeedForwardLayer(d_model, ff_dim, dropout_ratio)
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, src):
+
+        # src: [batch_size, src_len, d_model] 
+        # Layernorm (LN) is applied before every block
+        _src = self.layerNorm1(src)
+        _src, attention = self.multiHeadAttentionLayer(_src, _src, _src)
+
+        # residual connections after every block
+
+        # Dropout, when used, is applied after every dense layer except for the the qkv-projections
+        # and directly after adding positional- to patch embeddings. 
+        src = src + self.dropout(_src)
+        _src = self.layerNorm2(src)
+
+        # src: [batch_size, src_len, d_model]
+        _src = self.positionWiseFeedForward(_src)
+        src = src + self.dropout(_src)
+        # src: [batch_size, src_len, d_model]
+
+        return src, attention
+    
+class Encoder(nn.Module):
+    def __init__(self, d_model, n_layers, nhead, ff_dim, dropout_ratio):
+        super().__init__()
+        self.layers = nn.ModuleList([EncoderLayer(d_model, nhead, ff_dim, dropout_ratio) for _ in range(n_layers)])
+
+    def forward(self, src):
+
+        # src: [batch_size, src_len]
+        attentions = []
+        for layer in self.layers:
+            src, attention = layer(src)
+            attentions.append(attention)
+        # src: [batch_size, src_len, d_model]
+
+        return src, attentions
+    
+class ImageEmbedding(nn.Module):
+  def __init__(self, channel, patch_size, D):
+    super().__init__()
+    self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            nn.LayerNorm(channel*patch_size*patch_size),
+            nn.Linear(channel*patch_size*patch_size, D),
+            nn.LayerNorm(D),
+        )
+    # learnable cls_token (only for classification)
+    self.cls_token = nn.Parameter(torch.randn(1, 1, D))
+
+  def forward(self, image):
+
+    b, c, w, h = image.shape
+    # image: [batch_size, channel, width, height]
+    # n = num_w * num_h = (w*h) / (p1*p2)
+    embedded_patches = self.to_patch_embedding(image)
+    cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+    embedded_patches = torch.cat((cls_tokens, embedded_patches), dim=1)
+
+    return embedded_patches
+  
+
+class TokPosEmbedding(nn.Module):
+  def __init__(self, c, img_size, p, d_model, dropout_ratio):
+    super().__init__()
+    self.tokEmbedding = ImageEmbedding(c, p, d_model)
+    self.posEmbedding = nn.Embedding(100, d_model) 
+    self.d_model = d_model
+    self.dropout = nn.Dropout(dropout_ratio)
+    num_patches = (img_size[0] // p) * (img_size[1] // p)
+    self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, d_model))
+
+  def forward(self, src):
+
+    # src: [batch_size, width, height, channel]
+    src = self.tokEmbedding(src)
+    # src: [batch_size, src_len, d_model]
+
+    batch_size = src.shape[0]
+    src_len = src.shape[1]
+
+    src += self.pos_embedding[:, :src_len]
+    src = self.dropout(src)
+    # Dropout, when used, is applied after every dense layer except for the the qkv-projections and directly after adding positional- to patch embeddings
+
+    # src: [batch_size, src_len, d_model]
+
+    return src
+
+class VisionTransformer(nn.Module):
+    '''
+    ch : channel
+    img_size : image size
+    patch : patch size
+    d_model : embed dim
+    n_layers : num encode layers
+    n_head : num head
+    ff_dim : feed forward dim
+    dropout_ratio : dropout rate
+    output_dim : output dim
+    '''
+    def __init__(self, ch, img_size, patch, d_model, n_layers, n_head, ff_dim, dropout_ratio, output_dim):
+        super().__init__()
+
+        self.encEmbedding = TokPosEmbedding(ch, img_size, patch, d_model, dropout_ratio)
+        self.encoder = Encoder(d_model, n_layers, n_head, ff_dim, dropout_ratio)
+        self.layerNorm = nn.LayerNorm(d_model)
+        self.linear = nn.Linear(d_model, output_dim) 
+
+    def forward(self, src):    
+        # src: [batch_size, src_len]
+        src = self.encEmbedding(src)
+        enc_src, attentions = self.encoder(src)
+        # enc_src: [batch_size, src_len, d_model]
+        # classification head
+        cls_token = enc_src[:,0,:] # cls token
+        # cls_token: [batch_size, d_model]
+        cls_token = self.layerNorm(cls_token)
+        output = self.linear(cls_token)
+        # output: [batch_size, output_dim]
+
+        return output, attentions
---
+++ test.py
@@ -0,0 +1,233 @@
+import math, os, random, json, pickle, sys, pdb
+import string, shutil, time, argparse
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm as tqdm
+from einops import reduce
+
+import torch, torchvision
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+
+from data_loader import ImSituVerbGender
+from bias_only import VisionTransformer
+
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--save_dir', type=str,
+			help='path for saving checkpoints')
+	parser.add_argument('--ratio', type=str,
+			default = '0')
+	parser.add_argument('--num_verb', type=int,
+			default = 211)
+
+	parser.add_argument('--annotation_dir', type=str,
+			default='./data',
+			help='annotation files path')
+	parser.add_argument('--image_dir',
+			default = './data/of500_images_resized',
+			help='image directory')
+
+	parser.add_argument('--balanced', action='store_true',
+			help='use balanced subset for training')
+	parser.add_argument('--gender_balanced', action='store_true',
+			help='use balanced subset for training, ratio will be 1/2/3')
+	parser.add_argument('--batch_balanced', action='store_true',
+			help='in every batch, gender balanced')
+
+	parser.add_argument('--no_image', action='store_true',
+			help='do not load image in dataloaders')
+
+	parser.add_argument('--learning_rate', type=float, default=3e-2)
+	parser.add_argument('--finetune', action='store_true')
+	parser.add_argument('--num_epochs', type=int, default=20)
+	parser.add_argument('--batch_size', type=int, default=34)
+
+	parser.add_argument('--crop_size', type=int, default=224)
+	parser.add_argument('--image_size', type=int, default=256)
+	parser.add_argument('--start_epoch', type=int, default=1)
+	parser.add_argument('--seed', type=int, default=1)
+
+	args = parser.parse_args()
+
+	torch.manual_seed(args.seed)
+	torch.cuda.manual_seed(args.seed)
+
+	# image preprocessing
+	normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406],
+		std = [0.229, 0.224, 0.225])
+	val_transform = transforms.Compose([
+		transforms.Resize(args.image_size),
+		transforms.CenterCrop(args.crop_size),
+		transforms.ToTensor(),
+		normalize])
+
+	# Data samplers.
+	val_data = ImSituVerbGender(args, annotation_dir = args.annotation_dir, \
+			image_dir = args.image_dir, split = 'val', transform = val_transform)
+
+	# Data loaders / batch assemblers.
+	val_loader = torch.utils.data.DataLoader(val_data, batch_size = args.batch_size,
+			shuffle = False, num_workers = 4, pin_memory = True)
+
+	# build the models
+	channel = 3
+	patch_size = 16
+	d_model = 64
+	n_layers = 12
+	n_head = 8
+	ff_dim = 256
+	dropout_rate = 0.1
+	output_dim = 2
+	img_size = (val_loader.dataset[0][0].shape[0], val_loader.dataset[0][0].shape[1])
+
+	model = VisionTransformer(channel, img_size, patch_size, d_model, n_layers, n_head, ff_dim, dropout_rate, output_dim)
+	model = model.cuda()
+	model.eval()
+
+	if os.path.isfile(os.path.join(args.save_dir, 'model_best.pth.tar')):
+		print("=> loading checkpoint '{}'".format(args.save_dir))
+		checkpoint = torch.load(os.path.join(args.save_dir, 'model_best.pth.tar'))
+		args.start_epoch = checkpoint['epoch']
+		best_performance = checkpoint['best_performance']
+		model.load_state_dict(checkpoint['state_dict'])
+		print("=> loaded checkpoint (epoch {})".format(checkpoint['epoch']))
+	else:
+		print("=> no checkpoint found at '{}'".format(args.save_dir))
+		return None
+
+	# test(model, val_loader)
+
+	visualize_att(model,val_loader.dataset)
+
+def visualize_att(model,testdata):
+    denormalize = transforms.Normalize(mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],std=[1/0.229, 1/0.224, 1/0.225])
+    idxs = np.random.choice(range(len(testdata)), 5, False)
+    genders = ['male', 'female']
+    for idx in idxs:
+        img = testdata[idx][0]
+        label = np.argmax(testdata[idx][2].cpu().detach().numpy())
+        pred, attentions = model(img.unsqueeze(0).cuda())
+        pred = np.argmax(F.softmax(pred, dim=1).cpu().detach().numpy(), axis=1)[0]
+        
+        att_mat = torch.stack(attentions).squeeze(1)
+        att_mat = att_mat.cpu().detach()
+
+        att_mat = torch.mean(att_mat, dim=1)
+
+        residual_att = torch.eye(att_mat.size(1))
+        aug_att_mat = att_mat + residual_att
+        aug_att_mat = aug_att_mat / aug_att_mat.sum(dim=-1).unsqueeze(-1)
+
+        # Recursively multiply the weight matrices
+        joint_attentions = torch.zeros(aug_att_mat.size())
+        joint_attentions[0] = aug_att_mat[0]
+
+        for n in range(1, aug_att_mat.size(0)):
+            joint_attentions[n] = torch.matmul(aug_att_mat[n], joint_attentions[n-1])
+            
+        # Attention from the output token to the input space.
+        v = joint_attentions[-1]
+        grid_size = int(np.sqrt(aug_att_mat.size(-1)))
+        mask = v[0, 1:].reshape(grid_size, grid_size).detach().numpy()
+        mask = cv2.resize(mask / mask.max(), (img.shape[2], img.shape[1]))
+        img = denormalize(img)
+        img = np.transpose(img,(1,2,0))
+
+        flatten_mask = mask.reshape(-1)
+        print("mask_len {} / 20% {}".format(len(flatten_mask),int(0.9*len(flatten_mask))))
+        flatten_mask = np.sort(flatten_mask)
+        mask_val = flatten_mask[int(0.9*len(flatten_mask))]
+        print("max {} / min {} / val {}".format(mask.max(), mask.min(), mask_val))
+        
+        heatmap = cv2.applyColorMap(np.uint8(225*mask), cv2.COLORMAP_JET)
+        heatmap = np.float32(heatmap) / 255
+        result = heatmap + np.float32(img)
+        result = result / np.max(result)
+        result = np.uint8(255*result)
+
+        mask = mask[...,np.newaxis]
+        masked_img = np.where(np.repeat(mask, 3, axis=2)>mask_val, 0, img)
+
+        fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4, figsize=(12, 12))
+
+        ax1.set_title('Original')
+        ax2.set_title('Attention Mask')
+        ax3.set_title('Attention Map')
+        ax4.set_title('Masked image')
+        _ = ax1.imshow(img)
+        _ = ax2.imshow(mask)
+        _ = ax3.imshow(result)
+        _ = ax4.imshow(masked_img)
+        plt.savefig('img_{}.png'.format(idx))
+        
+        print("idx {}\n pred : {} // ground : {}".format(idx,genders[pred],genders[label]))
+        
+def test(model, val_loader):
+
+	# set the eval mode
+	model.eval()
+	nProcessed = 0
+	nVal = len(val_loader.dataset) # number of images
+	preds_list, truth_list = [], []
+
+	t = tqdm(val_loader)
+	for batch_idx, (images, targets, genders, image_ids) in enumerate(t):
+		#if batch_idx == 100: break # constrain epoch size
+
+		# Set mini-batch dataset
+		images = images.cuda()
+		genders = genders.cuda()
+
+		# Forward, Backward and Optimize
+		preds, _ = model(images)
+
+		preds = np.argmax(F.softmax(preds, dim=1).cpu().detach().numpy(), axis=1)
+		preds_list += preds.tolist()
+		truth_list += genders.cpu().max(1, keepdim=False)[1].numpy().tolist()
+		if batch_idx > 0 and len(preds_list) > 0:
+			total_genders = torch.cat((total_genders, genders.cpu()), 0)
+		else:
+			total_genders = genders.cpu()
+
+		# Print log info
+		nProcessed += len(images)
+
+	# compute accuracy
+	man_idx = total_genders[:, 0].nonzero().squeeze()
+	woman_idx = total_genders[:, 1].nonzero().squeeze()
+	acc = accuracy_score(truth_list, preds_list)
+
+
+	print('man size: {} woman size: {}'.format(len(man_idx), len(woman_idx)))
+	print('acc: {:.2f}'.format(acc))
+
+	return acc
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+if __name__ == '__main__':
+    main()
+
